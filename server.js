511943bc1ab4cf8348b721b91e18e12da120654e
Adam Doyle <adamdoyle630@gmail.com>
2022-11-28 13:46:56 -0500

    Added more endpoints

--- server.js
+++ server.js
@@ -1,24 +1,60 @@
 #!/usr/bin/env node
-import roll from "./lib/roll.js";
+import { roll } from "./lib/roll.js";
 import minimist from "minimist";
 import express from "express";
+
 
 const app = express();
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
-app.get('/app', (req, res, next) => {
+// Parse arguments using urlencoded
+app.use(express.urlencoded({extended: true}))
+
+// Default endpoint
+app.get('*', (req, res) => {
+    res.status(404)
+    res.send("404 NOT FOUND")
+})
+
+app.get('/app', (req, res) => {
     res.status(200);
     res.send("Status code : 200 OK");
 })
 
-app.get('/app/roll', (req, res, next) => {
-    res.status(200).json(roll.roll().end());
+app.get('/app/roll', (req, res) => {
+    res.status(200)
+    res.roll(6,2,1)
 })
 
-app.get('*', (req, res, next) => {
-    res.status(404)
-    res.send("404 NOT FOUND")
+app.post('/app/roll/', (req, res) => {
+    let sides = 6 || parseInt(req.body.sides);
+    let dice = 2 || parseInt(req.body.dice);
+    let rolls = 1 || parseInt(req.body.rolls);
+    res.status(200);
+    res.send(roll(sides, dice, rolls));
 })
+
+app.get('/app/roll/:sides/', (req, res) => {
+    let sides = parseInt(req.params.sides)
+    res.status(200)
+    res.send(roll(sides, 2, 1));
+})
+
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+    let sides = parseInt(req.params.sides)
+    let dice = parseInt(req.params.dice)
+    res.status(200)
+    res.send(roll(sides, dice, 1));
+})
+
+app.get('/app/roll/:sides/:dice/:/rolls/', (req, res) => {
+    let sides = parseInt(req.params.sides)
+    let dice = parseInt(req.params.dice)
+    let rolls = parseInt(req.params.rolls)
+    res.status(200)
+    res.send(roll(sides, dice, rolls));
+})
+
 
 console.log(JSON.stringify(roll()))
