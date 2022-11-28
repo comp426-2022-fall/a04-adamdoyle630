dbb8f0c23f808b87ab1aea53b9ae8734bfe3b5a8
Adam Doyle <adamdoyle630@gmail.com>
2022-11-28 13:25:02 -0500

    Started endpoints

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
-import roll from "./lib/a03/lib/roll.js";
+import roll from "./lib/roll.js";
 import minimist from "minimist";
 import express from "express";
 
-console.log(JSON.stringify(roll(1,2,3)))
+const app = express();
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+
+app.get('/app', (req, res, next) => {
+    res.status(200);
+    res.send("Status code : 200 OK");
+})
+
+app.get('/app/roll', (req, res, next) => {
+    res.status(200).json(roll.roll().end());
+})
+
+app.get('*', (req, res, next) => {
+    res.status(404)
+    res.send("404 NOT FOUND")
+})
+
+console.log(JSON.stringify(roll()))
