f92867544eeb2b37fca9cd92d4045b551e6ba574
Adam Doyle <adamdoyle630@gmail.com>
2022-11-28 13:59:19 -0500

    Fixed rolls endpoint

--- server.js
+++ server.js
@@ -13,48 +13,41 @@ app.use(express.urlencoded({extended: true}))
 
 // Default endpoint
 app.get('/app', (req, res) => {
-    res.status(200);
-    res.send("Status code : 200 OK");
+    res.status(200).send('200 OK');
 })
 
 app.get('/app/roll', (req, res) => {
-    res.status(200)
-    res.roll(6,2,1)
+    res.status(200).send(roll(6,2,1))
 })
 
 app.post('/app/roll/', (req, res) => {
     let sides = 6 || parseInt(req.body.sides);
     let dice = 2 || parseInt(req.body.dice);
     let rolls = 1 || parseInt(req.body.rolls);
-    res.status(200);
-    res.send(roll(sides, dice, rolls));
+    res.status(200).send(roll(sides, dice, rolls));
 })
 
 app.get('/app/roll/:sides/', (req, res) => {
     let sides = parseInt(req.params.sides)
-    res.status(200)
-    res.send(roll(sides, 2, 1));
+    res.status(200).send(roll(sides, 2, 1));
 })
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
     let sides = parseInt(req.params.sides)
     let dice = parseInt(req.params.dice)
-    res.status(200)
-    res.send(roll(sides, dice, 1));
+    res.status(200).send(roll(sides, dice, 1));
 })
 
-app.get('/app/roll/:sides/:dice/:/rolls/', (req, res) => {
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     let sides = parseInt(req.params.sides)
     let dice = parseInt(req.params.dice)
     let rolls = parseInt(req.params.rolls)
-    res.status(200)
-    res.send(roll(sides, dice, rolls));
+    res.status(200).send(roll(sides, dice, rolls));
 })
 
 // Post 404 if no endpoint found
 app.get('*', (req, res) => {
-    res.status(404)
-    res.send("404 NOT FOUND")
+    res.status(404).send('404 NOT FOUND')
 })
 
 app.listen(port, () => {
